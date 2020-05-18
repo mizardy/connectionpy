17b137706fe573ab96a0fa31c38a8efaec891dc0
mizardy <mizardy@protonmail.com>
2020-05-18 13:14:20 +0300

    add form panel

--- connection.py
+++ connection.py
@@ -2,6 +2,7 @@
 #create database
 #write code for connnection
 #write code for adding in table
+
 import pymysql
 from tkinter import *
 from tkinter import messagebox
@@ -14,27 +15,6 @@ connection = pymysql.connect(
     charset='utf8mb4',
     cursorclass=DictCursor
 )
-
-root = Tk()
-root.geometry("300x500")
-root.title("Войти в систему")
-
-
-def registration():
-    text = Label(text="Для входа в систему зарегистрируйтесь")
-    text_log = Label (text="Введите ваш логин:")
-    reg_login = Entry()
-    text_password = Label (text="Введите ваш пароль:")
-    reg_password = Entry(show="*")  
-    button_registration = Button(text="Зарегистрироваться")
-    text.pack()
-    text_log.pack()
-    reg_login.pack()
-    text_password.pack()
-    reg_password.pack()
-    button_registration.pack()
-registration()
-root.mainloop()
 
 addusers = connection.cursor()
 sql = "INSERT INTO users (id, login, password) VALUES (%s, %s,%s)"
---
+++ loginform.py
@@ -0,0 +1,20 @@
+root = Tk()
+root.geometry("300x600")
+root.title("Войти в систему Dev5")
+
+def registration():
+    text = Label(text="Для входа в систему зарегистрируйтесь")
+    text_log = Label (text="Введите ваш логин:")
+    reg_login = Entry()
+    text_password = Label (text="Введите ваш пароль:")
+    reg_password = Entry(show="*")  
+    button_registration = Button(text="Зарегистрироваться")
+    text.pack()
+    text_log.pack()
+    reg_login.pack()
+    text_password.pack()
+    reg_password.pack()
+    button_registration.pack()
+
+registration()
+root.mainloop()
